1393f315fda9acabd98333627f8507f07f637abc
Joe May <jmmay1974@gmail.com>
2020-07-14 13:51:23 -0700

    updated js and css

--- script.js
+++ script.js
@@ -1,42 +1,76 @@
-const citySearch = "Atlanta"
+const searchButton = document.getElementById("searchButton");
 const APIKey = "166a433c57516f51dfab1f7edaed8413";
 
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + APIKey;
+$("button").on("click", function(e){
+    e.preventDefault();
+    const citySearch = document.getElementById("search").value;
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + APIKey;
+    console.log(citySearch);
 
-
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(nowResult) {
+    
+    const cityName = (nowResult.name);
+    const cityTemp = (nowResult.main.temp);
+    const cityHumidity = (nowResult.main.humidity);
+    const cityWindspeed = (nowResult.wind.speed)
+    const cityID = (nowResult.id);
+    const cityLat = (nowResult.coord.lat);
+    const cityLon = (nowResult.coord.lon);
+    const uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
+    const fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" + cityID + "&units=imperial&appid=" + APIKey;
+    
+    
+    $('#city').text(cityName);
+    $('#temp').text(cityTemp + " degrees F");
+    $('#humidity').text(cityHumidity + "%");
+    $('#windspeed').text(cityWindspeed + " knots");
+    
+    
+    
+    
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(nowResult) {
-
-      
-      console.log(queryURL);
-      console.log(nowResult);
-      
-      const cityName = (nowResult.name);
-      const cityID = (nowResult.id);
-      console.log(cityName);
-      console.log(cityID);
-
-      const fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" + cityID + "&units=imperial&appid=" + APIKey;
-
-      
-    $.ajax({
-        url: fiveDayQueryURL,
+        url: uvQueryURL,
         method: "GET"
-    }).then(function(fiveDayResult){
-
-        console.log(fiveDayResult);
-        const day = new Date (fiveDayResult.list[i].dt);
+    }).then(function(uvResult){
+        
+        const cityUV = (uvResult.value)
+        $('#UV').text(cityUV);
+        
+        if (cityUV <= 2) {
+            $("#UV").addClass("lowUV");
+        } else if (cityUV >2 && cityUV <=5) {
+            $("#UV").addClass("moderateUV");
+        } else {
+            $("#UV").addClass("highUV");
+        }
+        
         
         // console.log(day);
         // console.log(day.getUTCDate());
         // console.log(day.toUTCString());
-
+        
     })
-
-      // Create CODE HERE to transfer content to HTML
-      // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-      // Create CODE HERE to dump the temperature content into HTML
-
-    });
+    
+    $.ajax({
+        url: fiveDayQueryURL,
+        method: "GET"
+    }).then(function(fiveDayResult){
+        
+        console.log(fiveDayResult);
+        const day = new Date (fiveDayResult.list[0].dt);
+        
+        // console.log(day);
+        // console.log(day.getUTCDate());
+        // console.log(day.toUTCString());
+        
+    })
+    
+    // Create CODE HERE to transfer content to HTML
+    // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+    // Create CODE HERE to dump the temperature content into HTML
+});
+    
+});
