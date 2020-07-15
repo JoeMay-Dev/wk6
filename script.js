e98be05a1a1650e47256d5e3885010a59fa048be
Joe May <jmmay1974@gmail.com>
2020-07-14 18:16:50 -0700

    added 5 day

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const searchButton = document.getElementById("searchButton");
 const APIKey = "166a433c57516f51dfab1f7edaed8413";
+const day = moment().format("MM/D/YY");
+console.log(day);
+let newDay = moment().add(1, 'days').format("MM/D/YY");
+console.log(newDay);
 
 $("button").on("click", function(e){
     e.preventDefault();
     const citySearch = document.getElementById("search").value;
     const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + APIKey;
     console.log(citySearch);
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(nowResult) {
     
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(nowResult) {
+        
+      
+
     const cityName = (nowResult.name);
     const cityTemp = (nowResult.main.temp);
     const cityHumidity = (nowResult.main.humidity);
@@ -20,10 +26,10 @@ $.ajax({
     const cityLat = (nowResult.coord.lat);
     const cityLon = (nowResult.coord.lon);
     const uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
-    const fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" + cityID + "&units=imperial&appid=" + APIKey;
+    const fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" + cityID + "&cnt=6&units=imperial&appid=" + APIKey;
     
     
-    $('#city').text(cityName);
+    $('#city').text(cityName + " " + day);
     $('#temp').text(cityTemp + " degrees F");
     $('#humidity').text(cityHumidity + "%");
     $('#windspeed').text(cityWindspeed + " knots");
@@ -45,12 +51,7 @@ $.ajax({
             $("#UV").addClass("moderateUV");
         } else {
             $("#UV").addClass("highUV");
-        }
-        
-        
-        // console.log(day);
-        // console.log(day.getUTCDate());
-        // console.log(day.toUTCString());
+        }    
         
     })
     
@@ -60,11 +61,22 @@ $.ajax({
     }).then(function(fiveDayResult){
         
         console.log(fiveDayResult);
-        const day = new Date (fiveDayResult.list[0].dt);
-        
-        // console.log(day);
-        // console.log(day.getUTCDate());
-        // console.log(day.toUTCString());
+        const fiveDay = fiveDayResult.list;
+
+        for (let i=1; i < fiveDay.length; i++){
+            const nextDiv = $("<div>");
+            const nextDate = $("<h4>");
+            const newDay = moment().add([i], 'days').format("MM/D/YY");
+            const maxTemp = $("<p>");
+            maxTemp.text("High: " + fiveDay[i].temp.max);
+            const minTemp = $("<p>");
+            minTemp.text("Low: " + fiveDay[i].temp.min);
+            console.log(newDay)
+            nextDate.text(newDay);
+            nextDiv.append(nextDate, maxTemp, minTemp);
+            $("#day" + [i]).prepend(nextDiv);
+        }
+
         
     })
     
